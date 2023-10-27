68ac1abef112fd9ef69916650ffd59fe8945744c
sikunrudra <sikunrudra2s@gmail.com>
2023-10-27 05:32:41 +0530

    modified the existing file

--- post-api.py
+++ post-api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+rom flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
-        return 'File uploaded successfully!'
+        return 'File downloaded successfully!'
     else:
         return 'No file selected.'
 
